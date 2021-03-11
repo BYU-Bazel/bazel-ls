174a14e47d8b087b8acae24e682e30b3e0906185
Ben Carlson <benocarlson@gmail.com>
2021-03-10 18:08:57 -0700

    commit changes

--- server/src/main/java/server/codelens/CodeLensProvider.java
+++ server/src/main/java/server/codelens/CodeLensProvider.java
@@ -34,18 +34,9 @@ public class CodeLensProvider {
     public CompletableFuture<List<? extends CodeLens>> getCodeLens(CodeLensParams params) {
         logger.info("CodeLens Provider invoked");
 
-<<<<<<< HEAD
         URI uri = new URI(params.getTextDocument().getUri());
         List<BuildTarget> targets = findTargets(uri);
         String contents = documentTracker.getContents(uri);
-=======
-        String contents = "";
-        try {
-            contents = documentTracker.getContents(new URI(params.getTextDocument().getUri()));
-        } catch (Exception e) {
-            logger.error(e);
-        }
->>>>>>> 0c036fd32503ad72bea5e3b73cad1693630f0354
 
         List<CodeLens> results = new ArrayList<>();
 
@@ -56,7 +47,6 @@ public class CodeLensProvider {
             results.add(result);
         }
 
-<<<<<<< HEAD
         // CodeLens dummy = new CodeLens();
         // Range range = new Range(new Position(4, 0), new Position(4, 13));
         // Command command = new Command();
@@ -67,8 +57,6 @@ public class CodeLensProvider {
 
         return CompletableFuture.completedFuture(results);
     }
-=======
->>>>>>> 0c036fd32503ad72bea5e3b73cad1693630f0354
 
     private List<BuildTarget> findTargets(URI uri) {
 
